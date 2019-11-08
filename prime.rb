4e5b2f1fe9be17f929bbf02c404d96db2717ce0e
Ivana <ivana_pile@yahoo.com>
2019-11-07 23:19:51 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+return true if number == 2 || number == 3
+return false if number == 1 || number % 2 == 0 || number % 3 == 0 
+i = 5
+j = 2
+while i*i <= number
+    return false if number % i == 0
+    i += j
+    j = 6 - j
+end
+return true
+end
