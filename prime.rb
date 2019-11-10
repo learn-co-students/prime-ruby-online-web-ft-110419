1d04f5c1b170d83b8b4145326027016b1703757b
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-11-10 23:04:13 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
+require 'pry'
 # Add  code here!
+def prime?(num)
+  if num < 0 || num == 0 || num == 1
+    return false
+  else
+    #binding.pry
+    (2..num-1).to_a.all?{|factor| num % factor != 0}
+  end
+end
